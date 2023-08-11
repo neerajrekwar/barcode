dda0d5f5f8b24a31ebee1c6063d166c307ca33df
neerajrekwar <neerajrekwar817@gmail.com>
2023-08-11 14:22:43 +0530

    df

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ function screen() {
 
 document.getElementById("screen-fit").innerHTML = 'Screen: ' + myWidth + "x" + myHeight + ' px';
 }
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+//     function time(){
+//     var d = new Date();
+//     var s = d.getSeconds();
+//     var m = d.getMinutes();
+//     var h = d.getHours();
+//     document.write(h + ":" + m + ":" + s);
+// }
+
+// setInterval(time,1000);
