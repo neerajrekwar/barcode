d152bf9e035d46b6a9605c63d4c73670b107217a
neerajrekwar <neerajrekwar817@gmail.com>
2023-08-16 01:15:58 +0530

    dd

--- script.js
+++ script.js
@@ -26,41 +26,4 @@ function checkTime(i) {
   return i;
 }
 
-//     function time(){
-//     var d = new Date();
-//     var s = d.getSeconds();
-//     var m = d.getMinutes();
-//     var h = d.getHours();
-//     document.write(h + ":" + m + ":" + s);
-// }
 
-// setInterval(time,1000);
-
-var randomPassword = function(length)
-{
-  chars = "!@abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-  var pass = "";
-  for(x=0;x<length;x++)
-  {
-    i = Math.floor(Math.random() * 62);
-    pass += chars.charAt(i);
-  }
-  $("#output").text( pass );
-}
-
-$("button").on("click", function() {
-  var length = $("input").val();
-  if (length <= 0) {
-    $("#output").show();
-    $("#output").text("Enter a number more than 0.");
-  }else{
-    $("#output").show();
-    randomPassword(length);
-  }
-  
-});
-
-$("#info").on("click", function() {
-  $("#output").show("");
-  $("#output").text("You can use the up and down arrow keys to change the value.");
-});
