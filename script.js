a17264890dd0ae0d5e5a9311ad8b2c7eb8f88618
neerajrekwar <neerajrekwar817@gmail.com>
2023-08-18 00:23:43 +0530

    gdf

--- script.js
+++ script.js
@@ -11,50 +11,7 @@ function screen() {
     "Screen: " + myWidth + "x" + myHeight + " px";
 }
 
-function startTime() {
-  const today = new Date();
-  let h = today.getHours();
-  let m = today.getMinutes();
-  let s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById("txt").innerHTML = h + ":" + m + ":" + s;
-  setTimeout(startTime, 1000);
-}
 
-function checkTime(i) {
-  if (i < 10) {
-    i = "0" + i;
-  } // add zero in front of numbers < 10
-  return i;
-}
-
-let pass = document.getElementById("pass");
-let btn = document.getElementById("btn");
-let btncp = document.getElementById("btncp");
-
-function generatePass() {
-  let chars =
-    "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+[]{}?><";
-  let passLength = document.getElementById('length-pass');
-  let password = "";
-
-  for (let i = 0; i < passLength; i++)
-    password += chars[Math.floor(Math.random() * chars.length)];
-
-  return password;
-}
-btn.addEventListener("click", () => {
-  pass.value = generatePass();
-});
-
-btncp.addEventListener("click", () => {
-  if (pass.value != "") {
-    navigator.clipboard.writeText(pass.value);
-    pass.classList.add("active");
-    setTimeout(() => {
-      pass.classList.remove("active");
-    }, 1000);
-  }
-});
-
+const date = new Date();
+let = date.getDate();
+document.getElementById('demo').innerHTML = date;
